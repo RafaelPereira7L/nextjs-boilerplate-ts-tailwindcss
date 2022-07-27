bf1004e083dfbc8de326cf5fb2457c7ca3e813ad
Rafael Pereira <rafaelhpmg@gmail.com>
2022-07-27 00:13:07 -0300

    :sparkles: Eslint and prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,18 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import ThemeChanger from '../components/themeChanger'
+import type { NextPage } from 'next';
+import React from 'react';
+import ThemeChanger from '../components/themeChanger';
 
 const Home: NextPage = () => {
   return (
-    <div className='flex items-center justify-center h-screen'>
+    <div className="flex items-center justify-center h-screen">
       <div className="flex text-3xl font-bold text-center gap-2">
-        <h1 className='text-zinc-900 dark:text-zinc-100'>Next.JS w/</h1>
-        <h1 className='text-blue-500'>TypeScript,</h1>
-        <h1 className='text-cyan-500'>TailwindCSS</h1>
-        <ThemeChanger/>
+        <h1 className="text-zinc-900 dark:text-zinc-100">Next.JS w/</h1>
+        <h1 className="text-blue-500">TypeScript,</h1>
+        <h1 className="text-cyan-500">TailwindCSS</h1>
+        <ThemeChanger />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
